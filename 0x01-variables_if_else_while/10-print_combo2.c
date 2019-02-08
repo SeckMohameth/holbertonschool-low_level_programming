11f03689cae26e3e35a9f3cd8f4f064e771b1287
SeckMohameth <mohamethseck34@gmail.com>
2019-02-08 23:49:06 +0000

    combo2

---
+++ 0x01-variables_if_else_while/10-print_combo2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print 00 to 99
+ * Return - returns 0
+ **/
+int main(void)
+{
+	int x;
+	int z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (z = '0'; z <= '9'; z++)
+		{
+			putchar(x);
+			putchar(z);
+
+			if (x < '9' || z < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
