2c5590933180d4fc952b7d848becd8d68a105f80
SeckMohameth <mohamethseck34@gmail.com>
2019-02-08 23:05:55 +0000

    print numbers with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	char  n;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 		if (n != '9')
 		{
 			putchar(',');
-		        putchar(' ');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
