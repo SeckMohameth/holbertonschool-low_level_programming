cf54fa623117908e28ccd43def18f7fd1f529822
SeckMohameth <mohamethseck34@gmail.com>
2019-02-08 22:05:09 +0000

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	char n;
 
-	for (n ='0'; n <= '9'; n++) 
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(n);
-		
+		putchar n);
+
 	}
 	putchar('\n');
-	return (0); 
+	return (0);
 }
