982b11d6cf20c571666469b914d2c5120394290f
SeckMohameth <mohamethseck34@gmail.com>
2019-02-11 21:56:41 +0000

    print without puts and printf

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print Holberton
+ * Return: returns 0
+ **/
+int main(void)
+{
+	char h = 'H';
+	char o = 'o';
+	char l = 'l';
+	char b = 'b';
+	char e = 'e';
+	char r = 'r';
+	char t = 't';
+	char x = 'o';
+	char n = 'n';
+
+
+	putchar (h);
+	putchar (o);
+	putchar (l);
+	putchar (b);
+	putchar (e);
+	putchar (r);
+	putchar (t);
+	putchar (x);
+	putchar (n);
+
+	putchar('\n');
+	return (0);
+}
