1fbacc85c49134eabf86b5c08da7ec429beca5f3
SeckMohameth <mohamethseck34@gmail.com>
2019-02-12 19:09:03 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - print alphabet
+ * Return: Always 0
+ **/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
