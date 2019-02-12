415f78ff9daf766838fc6b7b58399b71fa71caf5
SeckMohameth <mohamethseck34@gmail.com>
2019-02-12 23:22:35 +0000

    convert to absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _abs - print absolute value
- * @_abs: int n
+ * @n: int n
  * Return: returns 0
  **/
 int _abs(int n)
@@ -9,11 +9,11 @@ int _abs(int n)
 	if (n < 0)
 	{
 		return (n * -1);
-	} 
+	}
 	else if (n > 0)
 	{
 		return (1);
 	}
 
-	return (0);       
+	return (0);
 }
