b79a0c271d7559e09c8b7d2e53c77f1b5ac4b68b
SeckMohameth <mohamethseck34@gmail.com>
2019-02-14 22:27:28 +0000

    print 1-14 10 timnes _putchar

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* more_numbers - print 1-14 10 times
+* Return: returns 0
+**/
+void more_numbers(void)
+{
+	int n, x;
+
+	for (x = 0; x <= 10; x++ )
+	{
+
+	for (n = 0; n <= 14; n++)
+	{      
+		if (n > 9)
+		{
+			_putchar((n/10) + 48);
+		}
+	
+		_putchar((n%10) + 48);
+	}
+	
+	_putchar('\n');
+	}
+}
