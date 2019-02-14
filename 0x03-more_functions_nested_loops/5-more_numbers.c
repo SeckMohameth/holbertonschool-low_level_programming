f2e115e5f3b255abefdb7ae04b16f96962086951
SeckMohameth <mohamethseck34@gmail.com>
2019-02-14 22:30:10 +0000

    print 1-14 10 timnes _putchar

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,17 @@ void more_numbers(void)
 {
 	int n, x;
 
-	for (x = 0; x <= 10; x++ )
+	for (x = 0; x <= 10; x++)
 	{
 
 	for (n = 0; n <= 14; n++)
-	{      
+	{
 		if (n > 9)
 		{
-			_putchar((n/10) + 48);
+			_putchar((n / 10) + 48);
 		}
-	
-		_putchar((n%10) + 48);
+		_putchar((n % 10) + 48);
 	}
-	
 	_putchar('\n');
 	}
 }
