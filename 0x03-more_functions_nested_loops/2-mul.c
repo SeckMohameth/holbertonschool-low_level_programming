a211ef626fd902f221229073c76d176a634e91f2
SeckMohameth <mohamethseck34@gmail.com>
2019-02-14 15:49:40 +0000

    multiply two integers

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
 * mul - multiply 2 digits
-* @
-* Return: 
+* @a: int a
+* @b: int b
+* Return: returns a and b
 **/
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
