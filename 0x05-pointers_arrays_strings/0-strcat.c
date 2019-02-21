bbe06ed519a87609d8d572ff79afeaf5f9aaacd3
SeckMohameth <mohamethseck34@gmail.com>
2019-02-21 17:45:27 +0000

    combine two strings

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,26 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* _strcat - combine two strings
+* @dest: pointer first array string
+* @src: pointer to second string
+* Return: returns string
 **/
 char *_strcat(char *dest, char *src)
 {
 	int n, length;
+
 	n = 0;
 
 	for (length = 0; dest[length] != '\0'; length++)
 		;
-	
-	while(src[n] != '\0')
+
+	while (src[n] != '\0')
 	{
 		dest[length] = src[n];
 		n++;
 		length++;
 	}
-	
+
 	dest[length] = '\0';
 	return (0);
 }
