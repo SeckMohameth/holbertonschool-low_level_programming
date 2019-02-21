fa8942076149660da55dd7f2adf41a145fa97740
SeckMohameth <mohamethseck34@gmail.com>
2019-02-21 22:07:47 +0000

    new commit from local machine

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*
+*
+*
+**/
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	for (n = 0; s1[n] == s2[n] && s1[n] == '\0'; n++);
+		;
+
+		if (s1[n] < s2[n])
+		
+		if (s1[n] > s2[n])
+	       
+		       
+			
+		return (0);
+
+}
