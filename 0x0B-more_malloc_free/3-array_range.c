216d17f53b7077d85d377443c503fc2b65d7b273
SeckMohameth <mohamethseck34@gmail.com>
2019-03-08 18:26:58 +0000

    create array of ints min and max

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* array_range - create array of integers
+*@min: pointer to min string
+*@max: pointer to max
+*Return: array
+**/
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int *p;
+
+	int diff = (max - min) + 1;
+
+	if (max < min)
+		return (NULL);
+
+	p = array = malloc(sizeof(int) * diff);
+	if (min > max)
+		return (NULL);
+
+	while (min <= max)
+	{
+		*array = min;
+		min++;
+		array++;
+
+	}
+	return (p);
+
+}
