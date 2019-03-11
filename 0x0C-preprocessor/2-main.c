c3de3c4f33e8b17d7e1af5ad51c06bfb8d9270e8
SeckMohameth <mohamethseck34@gmail.com>
2019-03-11 17:56:00 +0000

    print name of file it was compiled from

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#ifndef _2_MAIN_H
+#define _2_MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - print name of file it was compiled
+* Return: returns 0
+**/
+
+int main(void)
+{
+
+printf("%s\n", __FILE__);
+
+
+return (0);
+}
+
+#endif
