c301283ea8a26b305401a4b8c146ab112198aabf
SeckMohameth <mohamethseck34@gmail.com>
2019-03-13 19:27:10 +0000

    print names

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_name - function that prints name
+* @name: name of person
+* @f: pointer to function
+* Return: returns nothing
+**/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	void print_name_as_is(char *name);
+	void print_name_uppercase(char *name);
+
+	f = &print_name_as_is;
+	f(name);
+        f = &print_name_uppercase;
+	f(name);
+
+}
