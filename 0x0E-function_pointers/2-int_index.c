c1f769f7f1727b9efba2f86e6145173178bd1121
SeckMohameth <mohamethseck34@gmail.com>
2019-03-13 23:23:58 +0000

    compare integers

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+* int_index - compare array elements
+*@array: array
+*@size: size of the array
+*@cmp: compare function
+*Return: returs int
+**/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+	}
+
+	return (array[i]);
+}
