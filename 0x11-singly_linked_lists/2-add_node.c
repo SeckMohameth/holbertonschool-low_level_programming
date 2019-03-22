6ba1450fa70c8a1cf50d94cfcb0e6e273638de0e
SeckMohameth <mohamethseck34@gmail.com>
2019-03-22 02:53:46 +0000

    print names

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -17,11 +17,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	if (new_node->next != '\0')
+	if (new_node->str != '\0')
+	{
 		for (i = 0; new_node->str[i] != '\0'; i++)
 		{
 			;
 		}
+	}
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
