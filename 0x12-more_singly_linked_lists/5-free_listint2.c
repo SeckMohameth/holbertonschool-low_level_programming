01f2587fcfe0b9b0ef7a44f2afb452881dc179f1
SeckMohameth <mohamethseck34@gmail.com>
2019-03-26 05:00:14 +0000

    free lists

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_listint2 - frees listint_t list
+* @head: pointer to head
+* Return: returns void
+**/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *mo;
+
+	while (ptr != NULL)
+	{
+		mo = ptr->next;
+		free(ptr);
+		ptr = mo;
+	}
+	*head = NULL;
+}
