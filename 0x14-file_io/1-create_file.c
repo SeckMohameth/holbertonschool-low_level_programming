934dd33871b3c25a27e30fcabe333e14ec2290f7
SeckMohameth <mohamethseck34@gmail.com>
2019-04-03 18:04:21 +0000

    fix 0file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* create_file - creates file
+* @filename: pointer to file
+* @text_content: Null terminated string to write file
+* Return: int
+**/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+
+}
