b4843e58a200ca1b4e304b7bbe9d8b91f03f8127
SeckMohameth <mohamethseck34@gmail.com>
2019-04-03 15:26:35 +0000

    read and print text file

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+/**
+* read_textfile - read text file and print to POSIX
+* @filename: pointer to file name
+* @letters: number of letters
+* Return: returns actual number of letters
+**/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char zard[1024];
+	int mo;
+	int belle;
+
+	if (filename == NULL)
+		return (0);
+
+
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+
+	mo = read(fd, zard, letters);
+
+
+	close(fd);
+
+
+
+	fd = open(filename, O_WRONLY);
+	if (fd == -1)
+		return (0);
+
+
+	belle = write(STDOUT_FILENO, zard, mo);
+	if (belle == -1)
+		return (0);
+
+
+	close(fd);
+
+	return (belle);
+}
