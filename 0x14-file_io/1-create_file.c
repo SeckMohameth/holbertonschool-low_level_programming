0f56d027134d7739c3d29b86741a9bf5d47dff32
SeckMohameth <mohamethseck34@gmail.com>
2019-04-04 02:20:22 +0000

    create a file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,5 +1,14 @@
 #include "holberton.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
 /**
 * create_file - creates file
 * @filename: pointer to file
@@ -8,10 +17,23 @@
 **/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, number, i = 0;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
 	if (filename == NULL)
 		return (-1);
 
+	while (text_content[i] != '\0')
+		i++;
+
+	number = write(fd, text_content, i);
+	if (number == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 
 }
