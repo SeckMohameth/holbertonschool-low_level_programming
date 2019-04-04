fbfb524298d6bef8e700c997f5ce62fed7785ee1
SeckMohameth <mohamethseck34@gmail.com>
2019-04-04 23:00:37 +0000

    fix with vim

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,6 +2,8 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *cap_string(char *);
+char *rot13(char *);
 
 
 int _strcmp(char *s1, char *s2);
