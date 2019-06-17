27732c57a4b9547ec0d370094e0d80ae56059d10
SeckMohameth <mohamethseck34@gmail.com>
2019-06-17 22:14:29 +0000

    algorithm project

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * swap - swap two integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: Nothing
+ **/
+
+void swap(int *num1, int *num2)
+{
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
+
+
+/**
+ * selection_sort - selection sort algorithm
+ * @array: pointer to array
+ * @size: size of array
+ * Return: Returns nothing
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t item1, item2, currentMin;
+
+	for (item1 = 0; item1 < size; item1++)
+	{
+		currentMin = item1;
+		for (item2 = item1 + 1; item2 < size; item2++)
+		{
+			if (array[item2] < array[currentMin])
+				currentMin = item2;
+			swap(&array[currentMin], &array[item1]);
+		}
+		print_array(array, size);
+	}
+}
---
+++ 0x1B-sorting_algorithms/README.md
@@ -0,0 +1,12 @@
+<h1> 0x1B. C - Sorting algorithms & Big O </h1>
+
+<h2>Learning Objectives </h2>
+
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+
+At least four different sorting algorithms
+What is the Big O notation, and how to evaluate the time complexity of an algorithm
+How to select the best sorting algorithm for a given input
+What is a stable sorting algorithm
