d0108f9516f432b2ce88e6770cae471493b6c3be
SeckMohameth <mohamethseck34@gmail.com>
2019-06-19 16:06:03 +0000

    less code

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -10,11 +10,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
-	unsigned long int c;
 
 	hash = hash_djb2(key);
-	c = hash % size;
-
-	return (c);
-
+	return (hash % size);
 }
