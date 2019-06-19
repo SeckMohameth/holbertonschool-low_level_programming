5dba7dc8651f937f5c9c2ac41e5f6e3fe8d8d0e2
SeckMohameth <mohamethseck34@gmail.com>
2019-06-19 01:48:49 +0000

    checking if min is not item1

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -14,7 +14,6 @@ void swap(int *num1, int *num2)
 	*num2 = temp;
 }
 
-
 /**
  * selection_sort - selection sort algorithm
  * @array: pointer to array
@@ -26,15 +25,21 @@ void selection_sort(int *array, size_t size)
 {
 	size_t item1, item2, currentMin;
 
-	for (item1 = 0; item1 < size; item1++)
+	for (item1 = 0; item1 < size - 1; item1++)
 	{
 		currentMin = item1;
 		for (item2 = item1 + 1; item2 < size; item2++)
 		{
 			if (array[item2] < array[currentMin])
+			{
 				currentMin = item2;
-			swap(&array[currentMin], &array[item1]);
+			}
 		}
-		print_array(array, size);
+		if (currentMin != item1)
+		{
+			swap(&array[currentMin], &array[item1]);
+			print_array(array, size);
+		}
+
 	}
 }
