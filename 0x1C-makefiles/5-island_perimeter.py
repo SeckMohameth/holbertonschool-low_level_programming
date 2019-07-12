2f9b3d79e65f83292b60a70987374e8ea16b6888
SeckMohameth <mohamethseck34@gmail.com>
2019-07-12 00:03:35 +0000

    islan prep question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,9 +3,44 @@
 
 
 def island_perimeter(grid):
-    p = 0
-    numberOfOnes = 0
+    numberOfZeros = 0
 
-    for idx in range(len(grid)):
-        p += sum(grid[idx]) * idx
-    return p
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column] == 1:
+
+                top = row - 1
+                left = column - 1
+                right = column + 1
+                bottom = row + 1
+
+                if top < 0:
+                    numTop = 0
+                else:
+                    numTop = grid[row - 1][column]
+
+                if left < 0:
+                    numLeft = 0
+                else:
+                    numLeft = grid[row][column - 1]
+
+                if right >= len(grid[0]):
+                    numRight = 0
+                else:
+                    numRight = grid[row][column + 1]
+
+                if bottom >= len(grid):
+                    numbottom = 0
+                else:
+                    numBottom = grid[row + 1][column]
+
+                if numTop is 0:
+                    numberOfZeros += 1
+                if numBottom is 0:
+                    numberOfZeros += 1
+                if numRight is 0:
+                    numberOfZeros += 1
+                if numLeft is 0:
+                    numberOfZeros += 1
+
+    return (numberOfZeros)
