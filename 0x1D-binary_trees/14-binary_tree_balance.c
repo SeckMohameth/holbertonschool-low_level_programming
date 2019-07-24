8f2142d097b4675be3dce44cae4d7dee80513aa5
SeckMohameth <mohamethseck34@gmail.com>
2019-07-24 18:08:39 +0000

    checking if balanced and then subtracts the difference

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,31 @@
 #include "binary_trees.h"
 
+
+/**
+ *
+ *
+ *
+ **/
+
+int isBalanced(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (-1);
+
+	left = isBalanced(tree->left) + 1;
+	right = isBalanced(tree->right) + 1;
+
+	if (left > right)
+		return (left);
+	else
+		return (right);
+
+}
+
+
+
 /**
  * binary_tree_balance - measures balance factor of a binary tree
  * @tree: tree to get the balance of
@@ -8,19 +34,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance;
+
+	int left, right;
+
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	if (tree->left == NULL)
-		return (-1);
-	if (tree->right == NULL)
-		return (1);
 
-	balance = binary_tree_balance(tree->left->left) - binary_tree_balance(
-		tree->right->right) + 1;
-	return (balance);
+	left = isBalanced(tree->left);
+	right = isBalanced(tree->right);
+
+	return (left - right);
+
 }
